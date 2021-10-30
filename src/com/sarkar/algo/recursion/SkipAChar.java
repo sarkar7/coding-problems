2bbbe68d87d9fc2b5f16b74324ad0bbee20b8daf
Sourabh Sarkar <sourabhsarkar7@gmail.com>
2021-10-30 18:45:19 +0530

    Recursion Problems.

---
+++ src/com/sarkar/algo/recursion/SkipAChar.java
@@ -0,0 +1,34 @@
+package com.sarkar.algo.recursion;
+
+public class SkipAChar {
+
+	
+	static String skipChar(String str, char skp) {
+		return skipItRecursively(str, 0, skp, new StringBuilder());
+	}
+	
+	
+
+	private static String skipItRecursively(String str, int i, char skp, StringBuilder sb) {
+		//Base Condition
+		if (str.length() - 1 == i) {
+			if (str.charAt(i) == skp) return sb.toString();
+			else return sb.append(str.charAt(i)).toString();
+		}
+		
+		if (str.charAt(i) == skp) {
+			return skipItRecursively(str, i+1, skp, sb);
+		} else {
+			return skipItRecursively(str, i+1, skp, sb.append(str.charAt(i)));
+		}
+		
+	}
+
+
+
+	public static void main(String[] args) {
+		String str = "baacfgahy";
+		System.out.println(skipChar(str, 'a'));
+	}
+
+}
---
+++ src/com/sarkar/algo/recursion/SubSetsOfString.java
@@ -0,0 +1,15 @@
+package com.sarkar.algo.recursion;
+
+public class SubSetsOfString {
+
+	public static void main(String[] args) {
+		String str = "abc";
+		System.out.println(subSets(str));
+	}
+
+	private static String subSets(String str) {
+		
+		return null;
+	}
+
+}
