f256b120923f95152c5db621f6b87fba2b822eea
Sourabh Sarkar <sourabhsarkar7@gmail.com>
2021-12-12 09:39:06 +0530

    Added LinkedList Runner File

---
+++ src/com/sarkar/algo/linkedlist/LinkedListDemo.java
@@ -0,0 +1,19 @@
+package com.sarkar.algo.linkedlist;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		
+		List<Integer> list = new LinkedList<>();
+		list.add(10);
+		list.add(21);
+		list.add(35);
+		
+		System.out.println(list.get(1));
+
+	}
+
+}
